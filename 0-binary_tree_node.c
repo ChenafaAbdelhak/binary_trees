5b18b0ad1c9d83e520c2c85eca51e12b2cb3e6c9
SIHAM <sihamfichouche@gmail.com>
2024-02-27 00:20:40 -0800

    Task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_node - Creates a binary tree node
  * @parent: A pointer to the parent of the node to create.
- * @value: The value to put in the new node.
+ * @value: The value of new node.
  *
  * Return: If an error occurs - NULL.
  *         Otherwise - a pointer to the new node.
@@ -11,7 +11,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
-	
+
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
