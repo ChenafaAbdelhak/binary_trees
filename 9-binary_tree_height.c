fd456f652bca0ac5781b6ce70df67ab4d9356b91
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-02-28 13:56:40 -0800

    task 9 -unfinished

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree.
+ * @tree: tree.
+ * Return: the height.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t h = 0;
+
+	h = (size_t) height(tree);
+	return (h);
+}
+
+/**
+ * height - function to calculate the height recursevly.
+ * @tree: the tree.
+ * Return: the height.
+ */
+
+int height(const binary_tree_t *tree)
+{
+	int leftHeight = 0, rightHeight = 0;
+
+	if (tree == NULL)
+		return (0);
+	else
+	{
+		leftHeight = height(tree->left);
+		rightHeight = height(tree->right);
+
+		return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
+	}
+}
