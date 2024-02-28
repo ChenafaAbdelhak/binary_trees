78a8b1ab1cad598b41b093e79a4aa58433c2d282
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-02-28 04:31:54 -0800

    task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function that deletes an entire binary tree
+ * @tree: the tree.
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *left_node, *right_node;
+
+	if (tree == NULL)
+		return;
+	
+	left_node = tree->left;
+	right_node = tree->right;
+
+	free(tree);
+	binary_tree_delete(left_node);
+	binary_tree_delete(right_node);
+}
---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/*
+ *
+ *
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *node;
+
+	if (tree == NULL)
+		return (NULL);
+}
