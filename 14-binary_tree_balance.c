7c6df53496adb167e289edbe4f84a13444dfa6e9
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-03-02 09:50:00 -0800

    fix header file

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,22 +13,3 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
-
-/**
- * binary_tree_height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the height.
- *
- * Return: If tree is NULL, your function must return 0, else return height.
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_height, right_height;
-
-	if (tree == NULL)
-		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return (1 + (left_height > right_height ? left_height : right_height));
-}
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,36 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - function that measures the height of a binary tree.
- * @tree: tree.
- * Return: the height.
+ *binary_tree_height - Measures the height of a binary tree.
+ *@tree: A pointer to the root node of the tree to measure the height.
+ *
+ *Return: If tree is NULL, your function must return 0, else return height.
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t h = 0;
-
-	if (tree == NULL)
-		return (h);
-
-	h = (size_t) height(tree);
-	return (h - 1);
-}
-
-/**
- * height - function to calculate the height recursevly.
- * @tree: the tree.
- * Return: the height.
- */
-
-int height(const binary_tree_t *tree)
-{
-	int leftHeight = 0, rightHeight = 0;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	leftHeight = height(tree->left);
-	rightHeight = height(tree->right);
-	return (1 + (leftHeight > rightHeight ? leftHeight : rightHeight));
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
